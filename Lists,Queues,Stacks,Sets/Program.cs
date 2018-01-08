785e0d9bf3c02080a739e2ec077ee9d2dd0af17a
MarcinB <marcio_br@interia.pl>
2018-01-08 17:00:11 +0100

    Add project files.

---
+++ Lists,Queues,Stacks,Sets/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists_Queues_Stacks_Sets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
